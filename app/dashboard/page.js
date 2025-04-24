7f6c97da5e9209850432a27066dc262d559c1c90
shahbaz-niporalo <shahbaz.k@nipralo.com>
2025-04-24 10:51:43 +0530

    fixerr

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,6 @@
 // pages/dashboard.js
 import { useUser } from '@auth0/nextjs-auth0/client';
-import { useSaveUserToStrapi } from '../hooks/useSaveUserToStrapi';
+import { useSaveUserToStrapi } from '@/hooks/useSaveUserToStrapi';
 
 export default function Dashboard() {
   const { user } = useUser();
